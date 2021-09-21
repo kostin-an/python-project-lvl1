3dc56cabe0c88060f8a8decd810c84f87dfbc18c
kostin-an <kostin-an@yandex.ru>
2021-09-21 14:46:27 +0300

    add step 3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python3
+
+from brain_games.cli import welcome_user
 
 def greething():
     print('Welcome to the Brain Games!')
 
 def main():
     greething()
+    welcome_user()
 
 if __name__ == '__main__':
     main()
